96e3d4d04893bda64a9cf11d1d765ef290f97a19
requires.io <support@requires.io>
2020-09-17 22:36:02 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
 ]
 REQUIREMENTS = [
-    'boto3>=1.14,<1.15',
+    'boto3>=1.15,<1.16',
     'celery>=4.4,<4.5',
     'kombu>=5.0,<5.1',
     'psycopg2>=2.7.1',
